275dee7393119d804eb249cc1fbb60b346060b81
ThakshilaDamsak <thakshiladamsak@hotmail.com>
2023-09-12 21:08:21 +0530

    modified:   common/footer.html 	modified:   error/404/content.js 	deleted:    home-css/background.html 	modified:   home-css/content.html 	modified:   home-css/content.js 	modified:   human/content.js 	modified:   index.html

--- error/404/content.js
+++ error/404/content.js
@@ -2,10 +2,10 @@
 fetch('/error/404/content.html')
     .then(response => response.text())
     .then(html => {
-    // Insert the footer HTML into the container
+    // Insert the content HTML into the container
     document.getElementById('page-content').innerHTML = html;
 
-    // Execute JavaScript code within the footer
+    // Execute JavaScript code within the container
     const scriptElements = Array.from(document.getElementById('page-content').getElementsByTagName('script'));
     scriptElements.forEach(script => {
         const newScript = document.createElement('script');
--- home-css/content.js
+++ home-css/content.js
@@ -1,15 +1,21 @@
-    // Fetch the content of the content HTML file
-fetch('/home-css/content.html')
-    .then(response => response.text())
-    .then(html => {
-    // Insert the footer HTML into the container
-    document.getElementById('page-content').innerHTML = html;
+  // Get the image element
+  const backgroundImage = document.getElementById('background-image');
 
-    // Execute JavaScript code within the footer
-    const scriptElements = Array.from(document.getElementById('page-content').getElementsByTagName('script'));
-    scriptElements.forEach(script => {
-        const newScript = document.createElement('script');
-        newScript.textContent = script.textContent;
-        document.body.appendChild(newScript);
-    });
-    });
+  // Add a load event listener
+  backgroundImage.addEventListener('load', () => {
+      // Fetch the content of the content HTML file
+      fetch('/home-css/content.html')
+          .then(response => response.text())
+          .then(html => {
+          // Insert the content HTML into the container
+          document.getElementById('page-content').innerHTML = html;
+
+          // Execute JavaScript code within the container
+          const scriptElements = Array.from(document.getElementById('page-content').getElementsByTagName('script'));
+          scriptElements.forEach(script => {
+              const newScript = document.createElement('script');
+              newScript.textContent = script.textContent;
+              document.body.appendChild(newScript);
+          });
+      });
+  });

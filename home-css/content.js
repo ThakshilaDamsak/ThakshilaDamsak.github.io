d6ce3e2430752db7d47f0f76ae955c416a1acf39
ThakshilaDamsak <thakshiladamsak@hotmail.com>
2023-09-13 15:54:24 +0530

    modified:   404.html 	modified:   common/footer.html 	modified:   common/header.css 	modified:   error/404/content.js 	modified:   home-css/content.js 	modified:   index.html

--- home-css/content.js
+++ home-css/content.js
@@ -1,21 +1,35 @@
-  // Get the image element
-  const backgroundImage = document.getElementById('background-image');
+// Get the container element for the images
+const imageContainer = document.getElementById('image-container');
 
-  // Add a load event listener
-  backgroundImage.addEventListener('load', () => {
-      // Fetch the content of the content HTML file
-      fetch('/home-css/content.html')
-          .then(response => response.text())
-          .then(html => {
-          // Insert the content HTML into the container
-          document.getElementById('page-content').innerHTML = html;
+// Get all the image elements inside the container
+const imageElements = Array.from(imageContainer.getElementsByTagName('img'));
 
-          // Execute JavaScript code within the container
-          const scriptElements = Array.from(document.getElementById('page-content').getElementsByTagName('script'));
-          scriptElements.forEach(script => {
-              const newScript = document.createElement('script');
-              newScript.textContent = script.textContent;
-              document.body.appendChild(newScript);
-          });
-      });
+// Create an array of promises for each image element
+const imagePromises = imageElements.map(image => {
+  // Return a new promise that resolves when the image loads
+  return new Promise(resolve => {
+    image.addEventListener('load', resolve);
   });
+});
+
+// Wait for all the promises to resolve
+Promise.all(imagePromises).then(() => {
+  // All the images have loaded
+  console.log('All images loaded');
+
+  // Fetch the content of the content HTML file
+  fetch('/home-css/content.html')
+    .then(response => response.text())
+    .then(html => {
+      // Insert the content HTML into the container
+      document.getElementById('page-content').innerHTML = html;
+
+      // Execute JavaScript code within the container
+      const scriptElements = Array.from(document.getElementById('page-content').getElementsByTagName('script'));
+      scriptElements.forEach(script => {
+        const newScript = document.createElement('script');
+        newScript.textContent = script.textContent;
+        document.body.appendChild(newScript);
+      });
+    });
+});

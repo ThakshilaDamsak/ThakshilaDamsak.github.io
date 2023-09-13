a4cd63b533547cdf443e64ada929fe525015aac7
ThakshilaDamsak <thakshiladamsak@hotmail.com>
2023-09-13 20:47:28 +0530

    boo

--- common/commons.js
+++ common/commons.js
@@ -1,39 +1,31 @@
 // Fetch the content of the footer HTML file
 fetch('/common/header.html')
-.then(response => response.text())
-.then(html => {
-  // Insert the footer HTML into the container
-  document.getElementById('header').innerHTML = html;
+    .then(response => response.text())
+    .then(html => {
+    // Insert the footer HTML into the container
+    document.getElementById('header').innerHTML = html;
 
-  // Show the footer after it is loaded
-  document.getElementById('header').style.display = 'block';
-
-  // Execute JavaScript code within the footer
-  const scriptElements = Array.from(document.getElementById('header').getElementsByTagName('script'));
-  scriptElements.forEach(script => {
-    const newScript = document.createElement('script');
-    newScript.textContent = script.textContent;
-    document.body.appendChild(newScript);
-  });
-});
-
+    // Execute JavaScript code within the footer
+    const scriptElements = Array.from(document.getElementById('header').getElementsByTagName('script'));
+    scriptElements.forEach(script => {
+        const newScript = document.createElement('script');
+        newScript.textContent = script.textContent;
+        document.body.appendChild(newScript);
+    });
+    });
 
 // Fetch the content of the footer HTML file
 fetch('/common/footer.html')
-  .then(response => response.text())
-  .then(html => {
+    .then(response => response.text())
+    .then(html => {
     // Insert the footer HTML into the container
     document.getElementById('footer').innerHTML = html;
-
-    // Show the footer after it is loaded
-    document.getElementById('footer').style.display = 'flex';
 
     // Execute JavaScript code within the footer
     const scriptElements = Array.from(document.getElementById('footer').getElementsByTagName('script'));
     scriptElements.forEach(script => {
-      const newScript = document.createElement('script');
-      newScript.textContent = script.textContent;
-      document.body.appendChild(newScript);
+        const newScript = document.createElement('script');
+        newScript.textContent = script.textContent;
+        document.body.appendChild(newScript);
     });
-  });
-
+    });

5fc8d85bccee6d8fb98554c7840d019c8166fcb9
ThakshilaDamsak <thakshiladamsak@hotmail.com>
2023-09-13 18:39:06 +0530

    modified:   404.html 	modified:   common/commons.js 	modified:   common/footer.html 	new file:   common/header.html 	new file:   common/themes.html 	modified:   human-old/index.html 	modified:   human/index.html 	modified:   index.html 	modified:   menu/index.html

--- common/commons.js
+++ common/commons.js
@@ -1,18 +1,18 @@
 // Fetch the content of the footer HTML file
-fetch('/common/menubar.html')
-.then(response => response.text())
-.then(html => {
-// Insert the footer HTML into the container
-document.getElementById('menubar').innerHTML = html;
+fetch('/common/header.html')
+    .then(response => response.text())
+    .then(html => {
+    // Insert the footer HTML into the container
+    document.getElementById('header').innerHTML = html;
 
-// Execute JavaScript code within the footer
-const scriptElements = Array.from(document.getElementById('menubar').getElementsByTagName('script'));
-scriptElements.forEach(script => {
-const newScript = document.createElement('script');
-newScript.textContent = script.textContent;
-document.body.appendChild(newScript);
-});
-});
+    // Execute JavaScript code within the footer
+    const scriptElements = Array.from(document.getElementById('header').getElementsByTagName('script'));
+    scriptElements.forEach(script => {
+        const newScript = document.createElement('script');
+        newScript.textContent = script.textContent;
+        document.body.appendChild(newScript);
+    });
+    });
 
 // Fetch the content of the footer HTML file
 fetch('/common/footer.html')

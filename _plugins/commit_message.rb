af25260deb36e34611fde219bb9df888dfe3f2b5
ThakshilaDamsak <thakshiladamsak@hotmail.com>
2023-11-07 21:54:22 +0530

    ooo baby uummmm yeah ooh! spider person is a fu- person ooh!

---
+++ _plugins/commit_message.rb
@@ -0,0 +1,7 @@
+require 'git'
+
+Jekyll::Hooks.register :site, :after_init do |site|
+  git = Git.open('.')
+  commit_message = git.log.first.message
+  File.write('_data/commit_message.yml', { 'message' => commit_message }.to_yaml)
+end

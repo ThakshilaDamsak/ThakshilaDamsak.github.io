175e73fb56334fd96deccb2779c20201e06601d8
ThakshilaDamsak <thakshiladamsak@hotmail.com>
2023-11-07 22:02:19 +0530

    boo?

--- _plugins/commit_message.rb
+++ _plugins/commit_message.rb
@@ -3,5 +3,5 @@ require 'git'
 Jekyll::Hooks.register :site, :after_init do |site|
   git = Git.open('.')
   commit_message = git.log.first.message
-  File.write('_data/commit_message.yml', { 'message' => commit_message }.to_yaml)
+  File.write('/_data/commit_message.yml', { 'message' => commit_message }.to_yaml)
 end

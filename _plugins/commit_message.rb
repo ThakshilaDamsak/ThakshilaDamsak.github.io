a7f2bc57e080a20c07ff2aa61e13cfe26f1dc9c7
ThakshilaDamsak <thakshiladamsak@hotmail.com>
2023-11-07 22:07:49 +0530

    eeeh! oo yeah responsibe spider robin person eating die!

--- _plugins/commit_message.rb
+++
@@ -1,7 +0,0 @@
-require 'git'
-
-Jekyll::Hooks.register :site, :after_init do |site|
-  git = Git.open('.')
-  commit_message = git.log.first.message
-  File.write('/_data/commit_message.yml', { 'message' => commit_message }.to_yaml)
-end

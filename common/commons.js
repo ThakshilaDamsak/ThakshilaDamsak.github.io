3a91b45fc245468d1ad492f364ce941924296249
ThakshilaDamsak <thakshiladamsak@hotmail.com>
2023-11-14 22:33:09 +0530

    great power point comes at the end of the century but it is not the end of the hot girls life 🤯

--- common/commons.js
+++ common/commons.js
@@ -1,5 +1,5 @@
 // Fetch the content of the header HTML file
-fetch('https://thakshiladamsak.github.io/common/header.html')
+fetch('/common/header.html')
     .then(response => response.text())
     .then(html => {
     // Insert the header HTML into the container
@@ -15,17 +15,23 @@ fetch('https://thakshiladamsak.github.io/common/header.html')
     });
 
   // Fetch the content of the social-links HTML file
-  fetch('https://thakshiladamsak.github.io/common/footer.html')
-    .then(response => response.text())
-    .then(html => {
-      // Insert the social-links HTML into the container
-      document.getElementById('footer').innerHTML = html;
+  fetch('/common/footer.html')
+  .then(response => response.text())
+  .then(html => {
+    // Insert the social-links HTML into the container
+    document.getElementById('footer').innerHTML = html;
 
-      // Execute JavaScript code within the container
-      const scriptElements = Array.from(document.getElementById('footer').getElementsByTagName('script'));
-      scriptElements.forEach(script => {
-        const newScript = document.createElement('script');
-        newScript.textContent = script.textContent;
-        document.body.appendChild(newScript);
+    // Execute JavaScript code within the container
+    const scriptElements = Array.from(document.getElementById('footer').getElementsByTagName('script'));
+    scriptElements.forEach(script => {
+      const newScript = document.createElement('script');
+      newScript.textContent = script.textContent;
+      // Add a load event listener to the new script element
+      newScript.addEventListener('load', function() {
+        // Execute the function that updates the content of the page
+        this.textContent();
       });
+      document.body.appendChild(newScript);
     });
+  });
+

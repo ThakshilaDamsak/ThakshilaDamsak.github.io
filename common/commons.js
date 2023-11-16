06babc796fe9895af93cfae961d79b92897e7f23
ThakshilaDamsak <thakshiladamsak@hotmail.com>
2023-11-16 16:50:50 +0530

    boom wwaahshsao

--- common/commons.js
+++ common/commons.js
@@ -1,18 +1,28 @@
-// Fetch the content of the header HTML file
-fetch('/common/header.html')
-    .then(response => response.text())
-    .then(html => {
-    // Insert the header HTML into the container
+//Get Header
+
+// Fetch the content of the social-links HTML file
+  fetch('/common/header.html')
+  .then(response => response.text())
+  .then(html => {
+    // Insert the social-links HTML into the container
     document.getElementById('header').innerHTML = html;
 
-    // Execute JavaScript code within the header
+    // Execute JavaScript code within the container
     const scriptElements = Array.from(document.getElementById('header').getElementsByTagName('script'));
     scriptElements.forEach(script => {
-        const newScript = document.createElement('script');
-        newScript.textContent = script.textContent;
-        document.body.appendChild(newScript);
+      const newScript = document.createElement('script');
+      newScript.textContent = script.textContent;
+      // Add a load event listener to the new script element
+      newScript.addEventListener('load', function() {
+        // Execute the function that updates the content of the page
+        this.textContent();
+      });
+      document.body.appendChild(newScript);
     });
-    });
+  });
+
+
+  //Get ooter
 
   // Fetch the content of the social-links HTML file
   fetch('/common/footer.html')
